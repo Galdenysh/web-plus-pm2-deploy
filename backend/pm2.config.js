c51e9c601e89f311764012fea1bb16f32edffecd
Igor Petrov <galdenysh@MacBook-Air-Igor.local>
2023-01-31 16:43:25 +0300

    feat: add pm2 deploy config

---
+++ backend/pm2.config.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF = 'origin/main',
+} = process.env;
+
+module.exports = {
+  apps: [{
+    name: 'api-mesto',
+    script: './dist/app.js',
+  }],
+
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: 'https://github.com/Galdenysh/web-plus-pm2-deploy.git',
+      path: DEPLOY_PATH,
+      'pre-deploy': `scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'post-deploy': 'npm i && npm run build',
+    },
+  },
+};
